1b2aff901d9cc0fd8bcc148f6209f00eb29db53f
John McCl <mars.john2018@gmail.com>
2018-10-03 22:16:24 -0400

    Change Network Device to enp0s3

--- mac_changer.py
+++ mac_changer.py
@@ -1,10 +1,11 @@
 #/usr/bin/env python
+
 # by John McClenon, Jr
 # With a little help from my friends
 
 import subprocess
 
-interface = eth0
+interface = enp0s3
 new_mac = "00:11:22:33:44:77"
 
 print("[+] Changing MAC address for " + interface
