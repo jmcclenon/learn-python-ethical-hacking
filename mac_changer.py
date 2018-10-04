fec13460a8f2d21e378b52b36a1b9b1bbba66756
John McCl <mars.john2018@gmail.com>
2018-10-04 13:11:29 -0400

    Change #5

--- mac_changer.py
+++ mac_changer.py
@@ -1,11 +1,16 @@
-# by John McClenon, Jr
-# With a little help from my friends
+#!/usr/local/bin/python
 
 import subprocess
 
-interface = "enp0s3"
+interface = 'enp0s3'
 new_mac = "00:11:22:33:44:77"
 old_mac = "08:00:27:a5:66:83"
+
+print(interface)
+print(new_mac)
+print(old_mac)
+
+
 
 print("[+] Changing MAC address for " + interface)
 
